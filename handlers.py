fff45caa704e58a99d5e7fe6f8c077b8608924af
Vl1ko <vfilipyan@list.ru>
2024-01-13 19:23:23 +0300

    com3

--- handlers.py
+++ handlers.py
@@ -10,19 +10,19 @@ async def start(message: types.Message):
                            reply_markup=keyboard)
 
 PRICE = {
-    '1': [types.LabeledPrice(label='Gift Card', amount=100000)],
-    '2': [types.LabeledPrice(label='Gift Card', amount=200000)],
-    '3': [types.LabeledPrice(label='Gift Card', amount=300000)],
-    '4': [types.LabeledPrice(label='Gift Card', amount=400000)],
-    '5': [types.LabeledPrice(label='Gift Card', amount=500000)],
-    '6': [types.LabeledPrice(label='Gift Card', amount=600000)],
+    '1': [types.LabeledPrice(label='Gift Card', amount=75000)],
+    '2': [types.LabeledPrice(label='Gift Card', amount=175000)],
+    '3': [types.LabeledPrice(label='Gift Card', amount=215000)],
+    '4': [types.LabeledPrice(label='Gift Card', amount=315000)],
+    '5': [types.LabeledPrice(label='Gift Card', amount=415000)],
+    '6': [types.LabeledPrice(label='Gift Card', amount=1015000)],
 }
 
 @dp.message(F.content_type == ContentType.WEB_APP_DATA)
 async def buy_process(web_app_message):
     await bot.send_invoice(web_app_message.chat.id,
-                           title='Laptop',
-                           description='Description',
+                           title='Подарочная карта Apple',
+                           description='Подарочная карта Apple',
                            provider_token='1832575495:TEST:e246626faf7f1f0f51d66db5eb1f87c96a3b988672a574e2b3b86c17bd555880',
                            currency='rub',
                            need_email=True,
