043229ed3061c2227a276156b84df9c5f3923548
Vl1ko <vfilipyan@list.ru>
2024-01-15 20:14:34 +0300

    com3

--- web/index.js
+++ web/index.js
@@ -7,6 +7,7 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
+let container = document.getElementById("container");
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
@@ -14,6 +15,12 @@ let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 let btn7 = document.getElementById("btn7");
+
+container.addEventListener("click", function() {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+})
 
 btn1.addEventListener("click", function() {
     if (tg.MainButton.isVisible) {
